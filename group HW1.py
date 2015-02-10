e047f3a3f7cbeaa7338fc0a6c026cbc94f569fa8
sjh9111 <samuel.hile@duke.edu>
2015-02-10 15:59:13 -0500

    remaining tasks: concat during import and DST days

--- group HW1.py
+++ group HW1.py
@@ -2,77 +2,51 @@ from __future__ import division
 from pandas import Series, DataFrame
 import pandas as pd
 import numpy as np
+import os
 
 #Pathing
 main_dir = "C:\Users\samjh_000"
 csv_file = "\Desktop\CES raw\SME and Residential allocations.csv"
-txt_file1 = "\Desktop\CES raw\File1.txt"
-txt_file2 = "\Desktop\CES raw\File2.txt"
-txt_file3 = "\Desktop\CES raw\File3.txt"
-txt_file4 = "\Desktop\CES raw\File4.txt"
-txt_file5 = "\Desktop\CES raw\File5.txt"
-txt_file6 = "\Desktop\CES raw\File6.txt"
+root = main_dir + "\Desktop\CES raw"
 
-#999999 values will be replaced with NaN upon import
+paths = [root + "\File" + str(v) + ".txt" for v in range(1,7)]
+
 missing = ['.', 'NA', 'NULL', '', '-', '9999999']
-header = ['ID', 'DayTime', 'consump']
 
-#Import data frames
-df1 = pd.read_table(main_dir + txt_file1, sep = "\s", na_values = missing)
-df2 = pd.read_table(main_dir + txt_file2, sep = "\s", na_values = missing)
-df3 = pd.read_table(main_dir + txt_file3, sep = "\s", na_values = missing)
-df4 = pd.read_table(main_dir + txt_file4, sep = "\s", na_values = missing)
-df5 = pd.read_table(main_dir + txt_file5, sep = "\s", na_values = missing)
-df6 = pd.read_table(main_dir + txt_file6, sep = "\s", na_values = missing)
+#Imports all dataframes as one giant list
+###TASK: add concatenate function into this line to save on load times 
+list_of_dfs = [pd.read_table(v, sep = " ", names = ['ID', 'DayTime', 'consump']) for v in paths]
 
-#Put headers on each df before stacking!
-df1.columns = header
-df2.columns = header
-df3.columns = header
-df4.columns = header
-df5.columns = header
-df6.columns = header
+#df_big = pd.concat([df1, df2, df3], ignore_index = True)
 
-#Stacks on stacks on stacks
-df_big = pd.concat([df1, df2, df3], ignore_index = True)
-
-#Drop full duplicates, going from top and bottom (might crash computer!)
+#Drop full row duplicates
 df_big = df_big.drop_duplicates()
 df_big = df_big.drop_duplicates(take_last = True)
 
 #Look for screwed up days
 df_big[df_big.DayTime == 45202]
 df_big[df_big.DayTime == 45203]
-df_big[df_big.DayTime == 66949]  
-df_big[df_big.DayTime == 66950]  
-df_big[df_big.DayTime == 29849]   
-df_big[df_big.DayTime == 29850]   
+df_big[df_big.DayTime == 66949]   #
+df_big[df_big.DayTime == 66950]   #
+df_big[df_big.DayTime == 29849]   #
+df_big[df_big.DayTime == 29850]   #
 
 #drop all records for DST days
+
+#example for filtering out days/times
+#a = 345999%100
+#day = ([] - a)/100
+
+
 #df_new = df1[df1.DayTime > 66900]
-#df1.DayTime < 67000]
-
-#######
-#Failing
-
-
-#df1.drop(df1['DayTime'] == 452**)
-#19943%100: last two
-#19943 - a (mod)/100
-
-#########
 
 
 #Drop NAs (including former 999999 entries
 df1.dropna()
 
-#remove entries w/999999 for consumption
 
 #import Excel data
-df_excel = pd.read_csv(main_dir + csv_file, usecols = ['ID', 'Code', 'Residential - Tariff allocation', 'Residential - stimulus allocation', 'SME allocation'])
+df_excel = pd.read_table(main_dir + csv_file, usecols = ['ID', 'Code', 'Residential - Tariff allocation', 'Residential - stimulus allocation', 'SME allocation'])
 
 #Merge Excel data based on meter IDs in common between sets
-df_merged = pd.merge(df1, df_excel) 
-## What to do about meter IDs w/no Excel labels (NaNs)?
-
-
+pd.merge(df1, df_excel) 
