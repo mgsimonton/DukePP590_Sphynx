31772e4b491ccdcb244f1e6cac7bc6e0f2da656f
sjh9111 <samuel.hile@duke.edu>
2015-02-11 08:00:40 -0500

    final version

--- group HW1.py
+++ group HW1.py
@@ -13,19 +13,14 @@ paths = [root + "\File" + str(v) + ".txt" for v in range(1,7)]
 
 missing = ['.', 'NA', 'NULL', '', '-', '9999999']
 
-#Imports all dataframes as one giant list
-###TASK: add concatenate function into this line to save on load times 
-#list_of_dfs = [pd.read_table(v, sep = " ", names = ['ID', 'DayTime', 'consump'], na_values = missing) for v in paths]
+#Imports and concatenates all textfiles into single dataframe
 df_big = pd.concat([pd.read_table(v, sep = " ", names = ['ID', 'DayTime', 'consump'], na_values = missing) for v in paths], ignore_index = True)
-
-
-#df_big = pd.concat([df1, df2, df3], ignore_index = True)
 
 #Drop full row duplicates
 df_big = df_big.drop_duplicates()
 df_big = df_big.drop_duplicates(take_last = True)
 
-#List of affected days
+####List of DST days
 #df_big[df_big.DayTime == 45202]
 #df_big[df_big.DayTime == 45203]
 #df_big[df_big.DayTime == 66949]   
@@ -33,7 +28,7 @@ df_big = df_big.drop_duplicates(take_last = True)
 #df_big[df_big.DayTime == 29849]   
 #df_big[df_big.DayTime == 29850]   
 
-#drop all records for DST days
+#drop all records w/hour numbers 49 and 50 (DST days)
 
 #example for filtering out days/times
 #a = 345999%100
@@ -42,10 +37,9 @@ df_big = df_big.drop_duplicates(take_last = True)
 #Drop NAs (including former 999999 entries
 df_big.dropna()
 
-
 #import Excel data
 df_excel = pd.read_table(main_dir + csv_file, usecols = ['ID', 'Code', 'Residential - Tariff allocation', 'Residential - stimulus allocation', 'SME allocation'])
 
 #Merge Excel data based on meter IDs in common between sets
 pd.merge(df_clean, df_excel) 
-#Leaving NaNs in for now
+#Leaving NaNs from Excel sheet in for now
--- group HW1_v2.py
+++ group HW1_v2.py
@@ -4,9 +4,6 @@ import pandas as pd
 import numpy as np
 import os
 
-
-
-# PATHING AND IMPORTING --------------
 # PATHING AND IMPORTING --------------
 
 main_dir = "C:\Users\samjh_000"
@@ -17,45 +14,32 @@ paths = [root + "\File" + str(v) + ".txt" for v in range(1,7)]
 missing = ['.', 'NA', 'NULL', '', '-', '9999999']
 
 #Imports all dataframes as one giant list
-###TASK: add concatenate function into this line to save on load times 
-#list_of_dfs = [pd.read_table(v, sep = " ", names = ['ID', 'DayTime', 'consump'], na_values = missing) for v in paths]
+
 df_big = pd.concat([pd.read_table(v, sep = " ", names = ['ID', 'DayTime', 'consump'], na_values = missing) for v in paths], ignore_index = True)
 
-#df_big = pd.concat([df1, df2, df3], ignore_index = True)
-
-
-
-# DROP DUPLICATES --------------
 # DROP DUPLICATES --------------
 
 #Drop full row duplicates
 df_big = df_big.drop_duplicates()
 df_big = df_big.drop_duplicates(take_last = True)
 
-
-
-# DROP DST ERRORS --------------
 # DROP DST ERRORS --------------
 
 df_big['Hour'] = df_big['DayTime']%100 
 # creates new column returning last 2 digits of DayTime (i.e., just the half hour indicator)
-df_big = df_big[(df_big.Hour <= 48)]
+df_big = df_big[(df_big.Hour <= 49)]
 # creates a new dataframe that omits the impossible half hour indicators 49 and 50
 test = df_big[(df_big.Hour >= 49)]
 test
 # 'test' list is empty, confirming that the 49 and 50 values are dropped from new DataFrame
 
 
-
-# DROP NAs --------------
 # DROP NAs --------------
 
 #Drop NAs (including former 999999 entries
 df_big.dropna()
 
 
-
-# MERGING DATASETS --------------
 # MERGING DATASETS --------------
 
 #import Excel data
